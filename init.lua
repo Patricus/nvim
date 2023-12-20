0ad2f04b5c1b0cf01f9116963c84c159a498c824
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2023-12-19 17:23:34 -0800

    Remove unused old WSL clipboard

--- init.lua
+++ init.lua
@@ -253,18 +253,6 @@ if WSL() then
     },
     cache_enabled = 0,
   }
-  -- vim.g.clipboard = {
-  --   name = 'WSLClipboard',
-  --   copy = {
-  --     ['+'] = 'clip.exe',
-  --     ['*'] = 'clip.exe',
-  --   },
-  --   paste = {
-  --     ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring():gsub("\\r", ""))',
-  --     ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring():gsub("\\r", ""))',
-  --   },
-  --   cache_enabled = 0,
-  -- }
 end
 
 -- Enable break indent
