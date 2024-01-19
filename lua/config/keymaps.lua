92b07d83562b8c5d2e06d2908014342d22db92cf
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2024-01-19 10:06:57 -0800

    Remove buffer save and delete

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -51,8 +51,6 @@ vim.keymap.set("n", "]q", "<cmd>cnext<cr>zz")
 -- Buffer navigation
 vim.keymap.set("n", "[b", "<cmd>bprev<cr>")
 vim.keymap.set("n", "]b", "<cmd>bnext<cr>")
-vim.keymap.set("n", "<c-s>", "<cmd>w<cr><cmd>bd<cr>") -- Write and delete Buffer
-vim.keymap.set("n", "<c-x>", "<cmd>bd<cr>")           -- Delete Buffer
 
 -- Resize windows
 vim.keymap.set("n", "<m-k>", "<c-w>+") -- Increase height
