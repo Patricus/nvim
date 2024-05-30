0859d84dd2003815982b323c0a84cfde568480a0
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2024-05-29 22:44:31 -0700

    Rustacean use mason for rust_analyzer

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -72,10 +72,10 @@ return {
             local servers = {
                 -- clangd = {},
                 -- gopls = {},
-                -- pyright = {},
-                -- rust_analyzer = {},
-                -- tsserver = {},
-                -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+                pyright = {},
+                rust_analyzer = {},
+                tsserver = {},
+                html = { filetypes = { 'html', 'twig', 'hbs' } },
 
                 lua_ls = {
                     Lua = {
@@ -118,5 +118,19 @@ return {
         'mrcjkb/rustaceanvim',
         version = '^4', -- Recommended
         lazy = false,   -- This plugin is already lazy
+        init = function()
+            vim.g.rustaceanvim = {
+                server = {
+                    cmd = function()
+                        local mason_registry = require('mason-registry')
+                        local ra_binary = mason_registry.is_installed('rust-analyzer')
+                            -- This may need to be tweaked, depending on the operating system.
+                            and mason_registry.get_package('rust-analyzer'):get_install_path() .. "/rust-analyzer"
+                            or "rust-analyzer"
+                        return { ra_binary } -- You can add args to the list, such as '--log-file'
+                    end,
+                },
+            }
+        end
     }
 }
