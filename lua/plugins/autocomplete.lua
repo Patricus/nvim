1708386de9578d4f6860a596b1f24b7b24dc36f6
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2024-06-11 13:50:47 -0700

    Remove rustaceannvim

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -96,6 +96,7 @@ return {
                 { name = 'buffer' },
             })
         })
+        require("cmp_git").setup()
 
         -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
         cmp.setup.cmdline({ '/', '?' }, {
@@ -112,7 +113,8 @@ return {
                 { name = 'path' }
             }, {
                 { name = 'cmdline' }
-            })
+            }),
+            matching = { disallow_symbol_nonprefix_matching = false }
         })
     end
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -101,36 +101,14 @@ return {
 
             mason_lspconfig.setup_handlers {
                 function(server_name)
-                    if server_name ~= "rust_analyzer" then
-                        require('lspconfig')[server_name].setup {
-                            capabilities = capabilities,
-                            on_attach = on_attach,
-                            settings = servers[server_name],
-                            filetypes = (servers[server_name] or {}).filetypes,
-                        }
-                    end
+                    require('lspconfig')[server_name].setup {
+                        capabilities = capabilities,
+                        on_attach = on_attach,
+                        settings = servers[server_name],
+                        filetypes = (servers[server_name] or {}).filetypes,
+                    }
                 end,
             }
         end
-
     },
-    {
-        'mrcjkb/rustaceanvim',
-        version = '^4', -- Recommended
-        lazy = false,   -- This plugin is already lazy
-        init = function()
-            vim.g.rustaceanvim = {
-                server = {
-                    cmd = function()
-                        local mason_registry = require('mason-registry')
-                        local ra_binary = mason_registry.is_installed('rust-analyzer')
-                            -- This may need to be tweaked, depending on the operating system.
-                            and mason_registry.get_package('rust-analyzer'):get_install_path() .. "/rust-analyzer"
-                            or "rust-analyzer"
-                        return { ra_binary } -- You can add args to the list, such as '--log-file'
-                    end,
-                },
-            }
-        end
-    }
 }
