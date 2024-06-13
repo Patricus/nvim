96b724c9366a2913bcfe5f919844b9faa957fcbc
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2024-06-13 10:49:32 -0700

    Oil view hidden files

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,7 +4,21 @@ return {
   keys = {
     { "-", "<cmd>Oil<cr>", { desc = "Open parent directory" } },
   },
-  opts = {},
+  opts = {
+    default_file_explorer = true,
+    delete_to_trash = true,
+    skip_confirm_for_simple_edits = true,
+    view_options = {
+      show_hidden = true,
+      natural_order = true,
+      is_always_hidden = function(name, _)
+        return name == '..' or name == '.git'
+      end,
+    },
+    win_options = {
+      wrap = true,
+    }
+  },
   dependencies = { "nvim-tree/nvim-web-devicons" },
   lazy = false,
 }
